0cb85f2ed0b0ee0be593e8043dcb22000a918f25
jzimmerman95 <jessicazimmerman95@gmail.com>
2014-11-09 21:26:56 -0500

    Added play game button and basic game view

--- src/edu/virginia/cs2110/MainActivity.java
+++ src/edu/virginia/cs2110/MainActivity.java
@@ -1,9 +1,12 @@
 package edu.virginia.cs2110;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +15,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+    }
+    
+    public void gameView(View view) {
+    	Intent intent = new Intent(this, GameActivity.class);
+    	startActivity(intent);
     }
 
 
