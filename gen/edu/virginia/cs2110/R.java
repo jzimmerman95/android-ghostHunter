55b48fa474ce29c4746e41a3e5992f3b8eee804c
jzimmerman95 <jessicazimmerman95@gmail.com>
2014-12-01 19:11:04 -0500

    background on main activity screen added

--- gen/edu/virginia/cs2110/R.java
+++ gen/edu/virginia/cs2110/R.java
@@ -28,9 +28,10 @@ public final class R {
         public static final int ghost=0x7f020003;
         public static final int ghsplash=0x7f020004;
         public static final int hauntedroom=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int leftcharacter=0x7f020007;
-        public static final int rightcharacter=0x7f020008;
+        public static final int hauntedroom2=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int leftcharacter=0x7f020008;
+        public static final int rightcharacter=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f09000b;
